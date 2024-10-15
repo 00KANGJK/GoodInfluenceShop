898a517b34877c80c68cd8e9c5f8b20f38260210
00KANGJK <kth00021000@gmail.com>
2024-10-15 13:18:03 +0900

    init

---
+++ src/main/java/com/goodinfluenceshop/GoodInfluenceShopApplication.java
@@ -0,0 +1,13 @@
+package com.goodinfluenceshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodInfluenceShopApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GoodInfluenceShopApplication.class, args);
+  }
+
+}
