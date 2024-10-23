b0754727cae6a5d5950438c193b4d24071b9c5b0
LimYeonKyuu <lim000606@naver.com>
2024-10-23 13:48:58 +0900

    feat: 공지사항 도메인 구성 완료

--- src/main/java/com/goodinfluenceshop/domain/Announcement.java
+++ src/main/java/com/goodinfluenceshop/domain/Announcement.java
@@ -1,17 +1,17 @@
 package com.goodinfluenceshop.domain;
 
 import com.goodinfluenceshop.domain.enums.AnnouncementCategory;
-import jakarta.persistence.Column;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
+import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 @Builder
-public class Announcement extends BaseEntity{
+public class Announcement extends BaseEntity {
 
     @Column(nullable = false)
     private String title;
@@ -25,4 +25,10 @@ public class Announcement extends BaseEntity{
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private AnnouncementCategory category;
+
+    @OneToMany(mappedBy = "announcement",
+            fetch = FetchType.LAZY,
+            cascade = CascadeType.ALL,
+            orphanRemoval = true)
+    private List<AnnouncementFile> announcementFiles;
 }
