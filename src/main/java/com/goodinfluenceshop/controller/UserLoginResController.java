dc21b5c00a14a4264854a62caafe75a5f69836ef
00KANGJK <kth00021000@gmail.com>
2024-12-18 11:31:14 +0900

    fix : 가게 로그인 Service 수정 #13

--- src/main/java/com/goodinfluenceshop/controller/UserLoginResController.java
+++ src/main/java/com/goodinfluenceshop/controller/UserLoginResController.java
@@ -4,6 +4,7 @@ import com.goodinfluenceshop.dto.StoreDto;
 import com.goodinfluenceshop.service.StoreService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 public class UserLoginResController {
+  @Autowired
   StoreService storeService;
 
   @PostMapping
