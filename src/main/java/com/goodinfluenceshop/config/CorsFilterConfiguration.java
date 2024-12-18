0552f94fd59746bdba4f54df5009e497c26b8c7f
00KANGJK <kth00021000@gmail.com>
2024-12-18 13:56:53 +0900

    fix : cors 에러 해결

--- src/main/java/com/goodinfluenceshop/config/CorsFilterConfiguration.java
+++ src/main/java/com/goodinfluenceshop/config/CorsFilterConfiguration.java
@@ -11,18 +11,22 @@ import java.util.Arrays;
 //헤더에 담을 내용도 확인!!
 @Configuration
 public class CorsFilterConfiguration {
-   @Bean
-   public CorsFilter corsFilter() {
-      UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-      CorsConfiguration config = new CorsConfiguration();
-      config.setAllowCredentials(true);
-      config.setAllowedOriginPatterns(Arrays.asList("*"));
-      config.addAllowedHeader("*");
-      config.addAllowedMethod("*");
-      String[] arrays = {"Authorization", "RefreshToken"};
-      config.setAllowedHeaders(Arrays.asList(arrays));
-      source.registerCorsConfiguration("/api/**", config);
-      config.setAllowedOrigins(Arrays.asList("http://localhost:3000","https://ginfluencer-admin-front.vercel.app"));
-      return new CorsFilter(source);
-   }
+  @Bean
+  public CorsFilter corsFilter() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowCredentials(true);
+    config.setAllowedOrigins(Arrays.asList(
+      "http://localhost:3000",
+      "https://ginfluencer-client-front.vercel.app",
+      "https://ginfluencer-admin-front.vercel.app"
+    ));
+    config.addAllowedHeader("*");
+    config.addAllowedMethod("*");
+    String[] arrays = {"Authorization", "RefreshToken"};
+    config.setAllowedHeaders(Arrays.asList(arrays));
+    source.registerCorsConfiguration("/api/**", config);
+    return new CorsFilter(source);
+  }
 }
+
